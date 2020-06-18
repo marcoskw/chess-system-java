2abb5867436740e5946e02b0b3799ccc9a1607ca
Marcos Korbes Werle <marcoskorbeswerle@hotmail.com>
2020-06-18 17:50:49 -0300

    1.10 - Correção na peça Rook e implementação de 'Possiveis caminhos da peça Rook'

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -25,6 +25,12 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	public boolean[][] possibleMoves(ChessPosition sourcePosition) {
+		Position position = sourcePosition.toPosition();
+		validadeSourcePosition(position);
+		return board.piece(position).possibleMoves();
+	}
+	
 	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
@@ -54,6 +60,7 @@ public class ChessMatch {
 		if (!board.piece(source).possibleMove(target)) {
 			throw new ChessException("The chosen piece can't move to target position");
 		}
+		
 	}
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
